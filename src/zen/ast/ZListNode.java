69e331047d9a7471e135bbb0113c248746189dea
kkuramitsu <kimio@konohascript.org>
2014-02-05 11:25:03 -0800

    adding ZListNode for handling variable number of elements

---
+++ src/zen/ast/ZListNode.java
@@ -0,0 +1,53 @@
+package zen.ast;
+
+import zen.deps.Field;
+import zen.deps.Var;
+import zen.parser.ZToken;
+
+public class ZListNode extends ZNode {
+	@Field public int ListStartIndex;
+	public ZListNode(ZNode ParentNode, ZToken SourceToken, int Size) {
+		super(ParentNode, SourceToken, Size);
+		this.ListStartIndex = Size;
+	}
+
+	public final void Append(ZNode Node) {
+		if(this.AST == null) {
+			this.AST = new ZNode[1];
+			this.Set(0, Node);
+		}
+		else {
+			@Var ZNode[] newAST = new ZNode[this.AST.length+1];
+			System.arraycopy(this.AST, 0, newAST, 0, this.AST.length);
+			this.AST = newAST;
+			this.Set(this.AST.length - 1, Node);
+		}
+	}
+
+	public final int GetListSize() {
+		if(this.AST == null) {
+			return 0;
+		}
+		return this.AST.length - this.ListStartIndex;
+	}
+
+	public final ZNode GetListAt(int Index) {
+		return this.AST[this.ListStartIndex + Index];
+	}
+
+	public final void SetListAt(int Index, ZNode Node) {
+		this.Set(Index + this.ListStartIndex, Node);
+	}
+
+	public final void ClearList() {
+		if(this.ListStartIndex == 0) {
+			this.AST = null;
+		}
+		else {
+			@Var ZNode[] newAST = new ZNode[this.ListStartIndex];
+			System.arraycopy(this.AST, 0, newAST, 0, this.ListStartIndex);
+			this.AST = newAST;
+		}
+	}
+
+}
