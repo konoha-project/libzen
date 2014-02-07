d109e16a572590e8cac83959c5629a9b526c9abe
Masahiro Ide <imasahiro9@gmail.com>
2014-02-07 16:38:02 +0900

    bug fixed in Setter syntax

--- src/libzen/grammar/SetterPattern.java
+++ src/libzen/grammar/SetterPattern.java
@@ -11,6 +11,7 @@ public class SetterPattern extends ZMatchFunction {
 		ZNode SetterNode = new ZSetterNode(ParentNode, LeftNode);
 		SetterNode = TokenContext.MatchToken(SetterNode, ".", ZTokenContext.Required);
 		SetterNode = TokenContext.MatchPattern(SetterNode, ZNode.NameInfo, "$Name$", ZTokenContext.Required);
+		SetterNode = TokenContext.MatchToken(SetterNode, "=", ZTokenContext.Required);
 		SetterNode = TokenContext.MatchPattern(SetterNode, ZSetterNode.Expr, "$Expression$", ZTokenContext.Required);
 		return SetterNode;
 	}
