99f955f77b89a4bc9874865c62043cecd349d3a2
Masahiro Ide <imasahiro9@gmail.com>
2014-02-17 17:42:41 +0900

    added testcases

---
+++ src/test/zen/generated/MethodDeclTest.java
@@ -0,0 +1,9 @@
+package test.zen.generated;
+import org.junit.Test;
+import zen.main.ZenMain;
+public class MethodDeclTest {
+    @Test
+    public void test() {
+        ZenMain.ExecCommand(new String[] {"-l", "jvm", "test/zen/Assign.zen"});
+    }
+}
