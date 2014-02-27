12f81e1bfa80a890a68659418b32329e0417c20e
kkuramitsu <kimio@konohascript.org>
2014-02-27 17:29:31 +0900

    adding ZenGenerator..

---
+++ src/zen/codegen/ZenGenerator.java
@@ -0,0 +1,13 @@
+package zen.codegen;
+
+import zen.parser.ZSourceGenerator;
+
+public class ZenGenerator extends ZSourceGenerator {
+
+	public ZenGenerator() {
+		super("zen", "Zen-0.1");
+	}
+
+
+
+}
