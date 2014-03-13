7efed3b643871a769e74564bc396845c0f1ecf68
kkuramitsu <kimio@konohascript.org>
2014-03-13 16:38:48 +0900

    missing files

---
+++ src/zen/grammar/OpenTypePatternFunction.java
@@ -0,0 +1,23 @@
+package zen.grammar;
+
+import zen.ast.ZNode;
+import zen.ast.ZTypeNode;
+import zen.parser.ZToken;
+import zen.parser.ZTokenContext;
+import zen.type.ZType;
+import zen.util.Var;
+import zen.util.ZMatchFunction;
+
+public class OpenTypePatternFunction extends ZMatchFunction {
+
+	@Override public ZNode Invoke(ZNode ParentNode, ZTokenContext TokenContext, ZNode LeftNode) {
+		@Var ZToken Token = TokenContext.GetToken(ZTokenContext._MoveNext);
+		@Var ZType Type = ParentNode.GetNameSpace().GetType(Token.GetText(), Token, true/*IsCreation*/);
+		if(Type != null) {
+			@Var ZTypeNode TypeNode = new ZTypeNode(ParentNode, Token, Type);
+			return TokenContext.ParsePatternAfter(ParentNode, TypeNode, "$TypeRight$", ZTokenContext._Optional);
+		}
+		return null; // Not Matched
+	}
+
+}
